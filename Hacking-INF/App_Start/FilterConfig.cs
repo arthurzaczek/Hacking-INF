2252cbc4a72a3e7aa54e55c2415f46c78dc6cb2e
Arthur Zaczek <zaczek@technikum-wien.at>
2016-11-21 14:39:48 +0100

    Initial commit

---
+++ Hacking-INF/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Hacking_INF
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Hacking-INF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hacking_INF.WebApiApplication" Language="C#" %>
