70a9437f1f8ab1265b0bb14f77dd5205d57f7851
Arthur Zaczek <zaczek@technikum-wien.at>
2016-11-25 10:51:03 +0100

    Syntax Highlighter

---
+++ Hacking-INF/app/editor.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+    selector: 'editor',
+    template: `<div [innerHTML]="code"></div>`
+})
+export class MarkdownComponent implements OnChanges, OnInit {
+    @Input() code: string = "";
+
+    ngOnInit(): void {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+    }
+}
--- Hacking-INF/app/markdown.component.ts
+++ Hacking-INF/app/markdown.component.ts
@@ -1,19 +1,36 @@
-﻿import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-
+﻿import { Component, Input, OnInit, OnChanges, SimpleChanges, AfterViewChecked, ElementRef } from '@angular/core';
+declare var jsHelper: any;
 @Component({
     selector: 'markdown',
     template: `<div [innerHTML]="convertedData"></div>`
 })
-export class MarkdownComponent implements OnChanges, OnInit {
+export class MarkdownComponent implements OnChanges, OnInit, AfterViewChecked {
     @Input() data: string = "";
     convertedData: string;
+
+    constructor(public element: ElementRef) {
+    }  
 
     ngOnInit(): void {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (this.data != null) {
-            this.convertedData = marked(this.data);
+            var renderer = new marked.Renderer();
+            renderer.table = function (header, body) {
+                return '<table class="table table-striped">' + header + body + '</table>';
+            };
+            renderer.code = function (code, language) {
+                var newCode = code.replace('\\stdin\{', '<span class="stdin">test</span>');
+                return '<pre><code>' + newCode + '</code></pre>'; 
+            };
+            this.convertedData = marked(this.data, {
+                renderer: renderer
+            });
         }
     }
+
+    public ngAfterViewChecked(): void {
+        jsHelper.initHighlightJS();
+    }  
 }
