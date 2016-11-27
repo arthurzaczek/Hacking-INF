17eded7b9f583bdd38ad25fefd3ab906865bfe9c
Arthur Zaczek <zaczek@technikum-wien.at>
2016-11-27 20:07:10 +0100

    Make use of BL, move examples one level lower

--- Hacking-INF/BL.cs
+++ Hacking-INF/BL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hacking_INF.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,9 +9,42 @@ using YamlDotNet.Serialization.NamingConventions;
 
 namespace Hacking_INF
 {
-    public static class BL
+    public class BL
     {
-        public static string ReadTextFile(string fileName)
+        public string ExamplesDir
+        {
+            get
+            {
+                return System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Examples");
+            }
+        }
+
+        public string GetExampleDir(string course, string name)
+        {
+            return Path.Combine(ExamplesDir, course, name);
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            var courses = ReadYAML<IEnumerable<Course>>(Path.Combine(ExamplesDir, "Info.yaml"));
+            return courses;
+        }
+
+        public IEnumerable<Example> GetExamples(string course)
+        {
+            return Directory.GetDirectories(Path.Combine(ExamplesDir, course))
+                .Select(dir =>
+                {
+                    var example = ReadYAML<Example>(Path.Combine(dir, "info.yaml"));
+                    example.Course = course;
+                    example.Name = Path.GetFileName(dir);
+                    return example;
+                });
+        }
+
+        
+
+        public string ReadTextFile(string fileName)
         {
             using (var sr = new StreamReader(fileName))
             {
@@ -18,7 +52,7 @@ namespace Hacking_INF
             }
         }
 
-        public static T ReadYAML<T>(string fileName)
+        public T ReadYAML<T>(string fileName)
         {
             using (var input = new StreamReader(fileName))
             {
--- Hacking-INF/Controllers/InfoController.cs
+++ Hacking-INF/Controllers/InfoController.cs
@@ -13,47 +13,38 @@ namespace Hacking_INF.Controllers
 {
     public class InfoController : ApiController
     {
+        private BL _bl = new BL();
+
         public IEnumerable<CourseViewModel> GetCourses()
         {
-            var courses = BL.ReadYAML<IEnumerable<Course>>(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Info.yaml"));
-            return courses.Select(i => new CourseViewModel(i));
+            return _bl.GetCourses().Select(i => new CourseViewModel(i));
         }
         public CourseViewModel GetCourse(string name)
         {
-            var courses = BL.ReadYAML<IEnumerable<Course>>(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Info.yaml"));
-            return new CourseViewModel(courses.Single(i => i.Name == name));
+            return new CourseViewModel(_bl.GetCourses().Single(i => i.Name == name));
         }
 
         public IEnumerable<ExampleViewModel> GetExamples(string course)
         {
-            return Directory.GetDirectories(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/" + course))
-                .Select(dir =>
-                {
-                    var example = BL.ReadYAML<Example>(Path.Combine(dir, "info.yaml"));
-                    example.Course = course;
-                    example.Name = Path.GetFileName(dir);
-                    return new ExampleViewModel(example);
-                });
+            return _bl.GetExamples(course).Select(i => new ExampleViewModel(i));
         }
 
         public ExampleViewModel GetExample(string course, string name)
         {
-            var dir = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/" + course + "/" + name);
-            var example = BL.ReadYAML<Example>(Path.Combine(dir, "info.yaml"));
-            example.Course = course;
-            example.Name = name;
+            var dir = _bl.GetExampleDir(course, name);
+            var example = _bl.GetExamples(course).Single(i => i.Name == name);
             var vmdl = new ExampleViewModel(example);
 
             var angabe = Directory.GetFiles(Path.Combine(dir, "text"), "*Angabe_full.md").FirstOrDefault();
             if (angabe != null)
             {
-                vmdl.Instruction = BL.ReadTextFile(angabe);
+                vmdl.Instruction = _bl.ReadTextFile(angabe);
             }
 
             var use_this_main = Path.Combine(dir, "src", "use_this_main.c");
             if (File.Exists(use_this_main))
             {
-                vmdl.SourceCode = BL.ReadTextFile(use_this_main);
+                vmdl.SourceCode = _bl.ReadTextFile(use_this_main);
             }
             else
             {
