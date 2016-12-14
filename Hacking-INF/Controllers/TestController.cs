168acc63b515080e938e50ea43d1a8fd982c27db
Arthur Zaczek <zaczek@technikum-wien.at>
2016-12-14 09:13:45 +0100

    remove unused using - System.Management does not exist on debian

--- Hacking-INF/Controllers/TestController.cs
+++ Hacking-INF/Controllers/TestController.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Management;
 using System.Net;
 using System.Net.Http;
 using System.Text;
