5a7fbf03d58da9cf75d1cc841f44aa65ae84a4e7
Arthur Zaczek <zaczek@technikum-wien.at>
2017-08-08 14:44:24 +0200

    show error message when update fails

--- Hacking-INF/app/admin-examples.component.ts
+++ Hacking-INF/app/admin-examples.component.ts
@@ -2,7 +2,8 @@
 import { Course } from './models';
 import { HackingService } from './hacking.service';
 
- import 'rxjs/Rx';
+import 'rxjs/Rx';
+declare var jsHelper: any;
 
 @Component({
     selector: 'admin-examples',
@@ -13,13 +14,21 @@ export class AdminExamplesComponent {
     constructor(
         private hackingService: HackingService) { }
 
-    repository: string = "defined in Web.config";
+    repository: string = "defined in App_Data/Settings/ExamplesRepo.yaml";
     updateResult: string = "";
+    isError: boolean = false;
 
     update(): void {
         var self = this;
+        this.updateResult = "";
+        this.isError = false;
+        jsHelper.showWaitDialog();
         this.hackingService.updateExamples().subscribe(result => {
             self.updateResult = result;
+            jsHelper.hideWaitDialog();
+        }, error => {
+            jsHelper.hideWaitDialog();
+            self.isError = true;
         });
     }
 }
